d0ac1b6013f1014f6c7090fb0231ef7634f3f4ae
JoelWakefield <joel.wakefield.dev@gmail.com>
2025-09-04 16:33:07 -0600

    organizing, formatting, modeling

---
+++ FormalRPG/FormalRPG.Client/ViewModels/Character.cs
@@ -0,0 +1,24 @@
+﻿
+
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FormalRPG.Client.ViewModels
+{
+    [Table("RpgCharacters")]
+    public class Character
+    {
+        public int Id { get; set; }
+        [NotNull]
+        [MaxLength(255)]
+        public string Name { get; set; } = "";
+        public int Xp { get; set; }
+        public int Money { get; set; }
+
+        public ICollection<Skill> Skills { get; set; } = [];
+        public ICollection<Quest> Quests { get; set; } = [];
+        public ICollection<Upgrade> Upgrades { get; set; } = [];
+        public ICollection<Item> Items { get; set; } = [];
+    }
+}
---
+++ FormalRPG/FormalRPG.Client/ViewModels/Upgrade.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FormalRPG.Client.ViewModels
+{
+    [Table("RpgUpgrades")]
+    public class Upgrade
+    {
+        public int Id { get; set; }
+        public int Level { get; set; }
+        public int Cost { get; set; }
+        [NotNull]
+        [MaxLength(255)]
+        public string Name { get; set; } = "";
+        [NotNull]
+        [MaxLength(1000)]
+        public string Description { get; set; } = "";
+
+        public ICollection<Character> Characters { get; set; } = [];
+    }
+}
